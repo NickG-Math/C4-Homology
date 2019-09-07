27f763e2cb4c73bb5dba438c8e3e919b33a0ef80
Nick G <54913349+NickG-Math@users.noreply.github.com>
2019-09-07 14:26:15 -0500

    Improved rankmult implementation

--- General/rankmult.m
+++ General/rankmult.m
@@ -1,21 +1,18 @@
-function rank=rankmult(a,b) %Finds the rank of (Z_{C_{a1}}+...+Z_{C_{an}}) box (Z_{C_{b1}}+...+Z_{C_{bm}})
-if size(a,2)==1 && size(b,2)==1
-    if a>b
-        rank=a*ones(1,b);
-    else
-        rank=b*ones(1,a);
-    end
+function rank=rankmult(a,b) %Finds the rank of Z_[Ca_1]+...+Z[Ca_n] tensor Z_[Cb_1]+...+Z[Cb_n]
+%a,b,rank are all row arrays
+if size(a,2)==1 && size(b,2)==1 %Quicker than going through the rest of the code
+   rank=max(a,b)*ones(1,min(a,b)); %Just math
 else
     rank=[];
     for i=1:size(a,2)
-        for j=1:size(b,2)
-            rank=[rank,rankmult(a(i),b(j))];
+        for j=1:size(b,2) %rank=[rank(a(1),b(1)), rank(a(1),b(2)),...]
+            rank=[rank,max(a(i),b(j))*ones(1,min(a(i),b(j)))];
         end
     end
 end
 
 
-% The nonvectorized version is actrually faster than
+% This preallocated version is actrually slower:
 %  helpcell=cell(size(a,2),size(b,2));
 %     for i=1:size(b,2)
 %         for j=1:size(a,2)
