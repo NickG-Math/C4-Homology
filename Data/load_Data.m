1c9b30866939e5c2731e683385dba571c085ed92
Nick G <54913349+NickG-Math@users.noreply.github.com>
2019-09-07 14:10:47 -0500

    Changed implementation of load_Data

--- Data/load_Data.m
+++ Data/load_Data.m
@@ -1,6 +1,6 @@
-clear
+function Data=load_Data()
 
-Data=struct();
+Data=struct(); %The variable that has all the data
 
 C4_Mackey_List=load('C4_Mackey_List.mat');
 Data.MackeyList=C4_Mackey_List.MackeyList;
@@ -12,5 +12,4 @@ Data.ChainsStandard=C4_Standard_Chains.ChainsStandard;
 C4_Change_Basis=load('C4_Change_Basis.mat');
 Data.ChangeBasis=C4_Change_Basis.ChangeBasis;
 
-
-clear C4_Mackey_List C4_Standard_Chains C4_Change_Basis;
+clear C4_Mackey_List C4_Standard_Chains C4_Change_Basis; %No reason to keep them in memory.
