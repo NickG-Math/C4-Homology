9c6c029b501276c1e588b1b8620e001ef139c430
Nick G <54913349+NickG-Math@users.noreply.github.com>
2019-09-07 20:53:59 -0500

    Update action.m

--- General/action.m
+++ General/action.m
@@ -1,4 +1,9 @@
-function gx=action(x,rank) %Takes the element x living in a group with rank and produces the element gx
+function gx=action(x,rank) 
+%Inputs: column x and row rank
+%Outputs: column gx of the same size as x
+%Description: Takes the element x living in a level of a free Mackey functor with given rank and
+%produces the element g*x where g is the generator of the group C2n
+
 gx=zeros(size(x,1),1);
 track=0;
 for i=1:size(rank,2)
