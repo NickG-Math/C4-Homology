4b0312a526759aaf28ade41e0a92a6ff2678bf22
Nick G <54913349+NickG-Math@users.noreply.github.com>
2019-09-07 21:59:38 -0500

    Update changeofbasis.m

--- General/changeofbasis.m
+++ General/changeofbasis.m
@@ -1,4 +1,9 @@
-function change=changeofbasis(A,B) %Given two bases A,B in matrix form, one being a permutation of the other, get the change of basis matrix from A to B
+function change=changeofbasis(A,B) 
+%Inputs: Matrices A,B
+%Outputs: Logical matrix change
+%Description: Given two bases A,B in matrix form turn then into arrays (the actual bases) and get the change of basis matrix from A to B
+%This only works if one matrix is a permutation of the other.
+
 %We first convert the bases into arrays
 if size(A,1)>1
     A=A';
@@ -10,5 +15,6 @@ if size(B,1)>1
     B=B(:);
     B=B';
 end
-change=(B'==A);
+%We put 1 at position (i,j) when B(j)=A(i)
+change=(B'==A); %Very fast vectorized version
 end
