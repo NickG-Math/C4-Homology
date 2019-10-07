84b6948bb078bd73b03b129bb8b21e1970719374
Nick G <54913349+NickG-Math@users.noreply.github.com>
2019-10-07 01:26:40 -0500

    O(n) complexity in changeofbasis

--- General/changeofbasis.m
+++ General/changeofbasis.m
@@ -24,11 +24,24 @@ if size(B,1)>1
     B=B';
 end
 %We put 1 at position (i,j) when B(j)=A(i)
+%First compute the inverse of the permutation A
+maximum=max(A);
+C=zeros(1,maximum+1);
+for i=1:size(A,2)
+    C(A(i)+1)=i;
+end
 A_to_B=zeros(1,size(A,2));
-for i=1:size(A_to_B,2)
-    A_to_B(i)=find(B(i)==A,1,'first');
+for i=1:size(A,2)
+    A_to_B(i)=C(B(i)+1);
 end
-end
+
+% 
+% A_to_B=zeros(1,size(A,2));
+% for i=1:size(A_to_B,2)
+%     A_to_B(i)=find(B(i)==A,1,'first');
+% end
+% end
+
 %If the permutation matrix is desired instead, change=double(B'==A); This is slightly faster
 %Comparison of the two methods:
 %change*U=U(A_to_B,:)
